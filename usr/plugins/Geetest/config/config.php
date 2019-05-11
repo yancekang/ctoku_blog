ea8f5f00badf1c428d1152d73091f5bf5ca32251
yangon <150018405@qq.com>
2019-05-12 03:19:53 +0800

    up

---
+++ usr/plugins/Geetest/config/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mr.sin
+ * Date: 2017/4/9
+ * Time: 下午10:23
+ */
+@define("CAPTCHA_ID", $options->CAPTCHA_ID);
+@define("PRIVATE_KEY", $options->PRIVATE_KEY);
+?>
