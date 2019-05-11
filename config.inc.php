81edae8259984bb73fd3eb77962ae2e2535074e3
yangon <150018405@qq.com>
2019-05-12 03:50:07 +0800

    up

--- config.inc.php
+++ config.inc.php
@@ -55,10 +55,10 @@ Typecho_Common::init();
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
   'host' => 'localhost',
-  'user' => 'ctokublog',
-  'password' => '5yPJYnEPnzWJEawf',
+  'user' => 'blog',
+  'password' => 'jLkbcZ7WnyDE6sfz',
   'charset' => 'utf8',
   'port' => '3306',
-  'database' => 'ctokublog',
+  'database' => 'blog',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
