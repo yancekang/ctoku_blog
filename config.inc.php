e7ec981dbbc4f60022b129b11687e65eed789157
yangjianxin <150018405@qq.com>
2019-06-05 10:25:40 +0800

    up

--- config.inc.php
+++ config.inc.php
@@ -54,7 +54,7 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '47.93.235.72',
+  'host' => 'localhost',
   'user' => 'ctokublog',
   'password' => '5yPJYnEPnzWJEawf',
   'charset' => 'utf8',
