106c804882e3db0532772ff574396bddfad68362
ctoku_server <150018405@qq.com>
2019-06-05 09:13:39 +0800

    up

---
+++ usr/plugins/WeiboSync/config.php
@@ -0,0 +1,4 @@
+<?php
+define( 'WB_AKEY' , '481944186' );
+define( 'WB_SKEY' , '758fe29a9fca92fc3af2705dff290f8a' );
+define( 'WB_CALLBACK_URL' , 'https://www.ctoku.com/weibo/index.php' );
---
+++ weibo/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '481944186' );
+define( "WB_SKEY" , '758fe29a9fca92fc3af2705dff290f8a' );
+define( "WB_CALLBACK_URL" , 'https://www.ctoku.com/weibo/index.php' );
